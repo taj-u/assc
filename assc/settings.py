8034f2e1baaa90ec5efdc05f165436c4f0adec27
Taj Uddin <tajcsebsmrstu@gmail.com>
2021-10-31 10:58:30 +0600

    updating

--- assc/settings.py
+++ assc/settings.py
@@ -124,6 +124,14 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    "/mab/static",
+]
+
+# Media files
+
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- assc/urls.py
+++ assc/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('mab.urls', namespace = 'home_page')),
+    path('', include('mab.urls', namespace='mab')),
     path('admin/', admin.site.urls),
-] + staticfiles_urlpatterns()
+    path('blog/', include('blog.urls', namespace="blog"))
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
